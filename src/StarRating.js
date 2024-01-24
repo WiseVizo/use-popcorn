2b4ed3f4b58807c6d1631b19f8995442cd0b7bc1
WiseVizo <mecoc1011@gmail.com>
2024-01-24 16:19:36 +0530

    created starRating component
    for movie ratings

---
+++ src/StarRating.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import "./StarRating.css";
+
+const StarRating = ({ maxRating }) => {
+  const [rating, setRating] = useState(0);
+  const [clickedRating, setClickedRating] = useState(0);
+
+  const handleHover = (hoverRating) => {
+    setRating(hoverRating);
+  };
+
+  const handleClick = (clickedRating) => {
+    setRating(clickedRating);
+    setClickedRating(clickedRating);
+  };
+
+  function handleMoseLeave() {
+    setRating(0);
+    setRating(clickedRating);
+  }
+
+  return (
+    <div className="star-rating-container">
+      {[...Array(maxRating).keys()].map((star) => (
+        <span
+          key={star}
+          className={star + 1 <= rating ? "star-filled" : "star"}
+          onMouseEnter={() => handleHover(star + 1)}
+          onMouseLeave={() => handleMoseLeave()}
+          onClick={() => handleClick(star + 1)}
+        >
+          &#9733; {/* Unicode character for a star */}
+        </span>
+      ))}
+      <p className="rating-text">Rating: {rating}</p>
+    </div>
+  );
+};
+
+export default StarRating;
